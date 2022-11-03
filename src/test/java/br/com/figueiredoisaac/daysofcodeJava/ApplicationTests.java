21c7e27ac95e56463eabaa0cf8a9443aaf0a9ce1
Isaac Figueiredo <figueiredoisaac@gmail.com>
2022-11-03 01:42:24 -0300

    Commit inicial de projeto para consumir Api do IMDB

---
+++ src/test/java/br/com/figueiredoisaac/daysofcodeJava/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.figueiredoisaac.daysofcodeJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
